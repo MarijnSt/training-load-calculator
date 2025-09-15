70556fae9dfa2ea41ce2bb2e075b19eeb3832e11
MarijnSt <marijn.stammeleer@gmail.com>
2025-09-15 16:30:14 +0200

    feat: RPE info expander

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ import streamlit as st
 
 from src.components.session_table import create_session_table
 from src.components.training_chart import create_training_chart
+from src.components.rpe_info import rpe_info
 
 st.title("Training Load Tool")
 st.write("")
 st.write("Gelieve drills toe te voegen aan de tabel hieronder om de training load voor deze sessie te berekenen.")
 st.write("")
+
+# RPE info
+rpe_info()
 
 # Session table
 session_df = create_session_table()
---
+++ src/components/rpe_info.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def rpe_info():
+    with st.expander("RPE info"):
+        st.image("static/rpe.png")
+    
+    st.write("")
